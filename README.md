fec918272e90193d0e08d32d02fcaf525e1c29c4
Evangeline VIllena <vvanjie@yahoo.com>
2018-02-21 14:34:24 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Beatles Loops
+d# Beatles Loops
 
 ## Objectives
 + Build a for loop
---
+++ index.js
@@ -0,0 +1,35 @@
+function theBeatlesPlay(musicians, instruments){
+var text = [];
+for (var m = 0; m < musicians.length; m++) {
+    text.push(`${musicians[m]} plays ${instruments[m]}`);
+} 
+return text;
+}
+
+theBeatlesPlay('John Lennon', 'Guitar');
+
+function johnLennonFacts(facts){
+  
+var i = 0;
+var newFacts = [];
+while (i < facts.length){
+   newFacts.push(facts[i] + "!!!");
+   i++;
+  }
+  return newFacts;
+} 
+
+johnLennonFacts("He was the last Beatle to learn to drive");
+
+
+function iLoveTheBeatles(number) {
+    var text = [];
+    do {
+        text.push("I love the Beatles!");
+        number++;
+    } while (number < 15);
+
+    return text;
+}
+
+iLoveTheBeatles(7);
