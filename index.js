cc509f3d5391f980767bf56d82f942d6854e823e
Evangeline VIllena <vvanjie@yahoo.com>
2018-02-21 14:33:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,12 +9,7 @@ return text;
 theBeatlesPlay('John Lennon', 'Guitar');
 
 function johnLennonFacts(facts){
-  var facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
+  
 var i = 0;
 var newFacts = [];
 while (i < facts.length){
