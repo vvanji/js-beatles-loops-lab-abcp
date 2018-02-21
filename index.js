812588aab971f94d0086634049160f44e96cd562
Evangeline VIllena <vvanjie@yahoo.com>
2018-02-21 13:56:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,13 +8,18 @@ return text;
 
 theBeatlesPlay('John Lennon', 'Guitar');
 
-function johnLennonFacts() {
-    var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
-    var newFacts = [];
-    var i = 0;
-    while (i < facts.length) {
-        newFacts.push(facts[i] +"!!!");
-        i++;
-    }
-    return newFacts;
-}
+function johnLennonFacts(){
+  var facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+var i = 0;
+var newFacts = [];
+while (i < facts.length){
+   newFacts.push(`${facts[i]} !!!`);
+   i++;
+  }
+  return newFacts;
+} 
